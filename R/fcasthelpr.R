d499cf3019729fc971daffec9b33af0f3434064f
Brian Gulbis <brian.gulbis@gmail.com>
2020-09-03 13:13:28 -0500

    add package imports

---
+++ R/fcasthelpr.R
@@ -0,0 +1,13 @@
+#' Hepler functions for forecast models
+#'
+#' \pkg{fcasthelpr} provides functions to facilitate creating and visualizing
+#' forecasting models.
+#'
+#' This package provides functions for creating and visualizing forecasting
+#' models.
+#'
+#' @docType package
+#' @name fcasthelpr
+#' @importFrom magrittr %>%
+#' @importFrom rlang :=
+NULL
