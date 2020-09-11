353fbd9b990401c71206336946fba0bb8a7d9482
Brian Gulbis <brian.gulbis@gmail.com>
2020-09-11 12:45:03 -0500

    remove unnecessary functions

--- R/data_functions.R
+++ R/data_functions.R
@@ -30,6 +30,7 @@ calc_params <- function(.data) {
         )
 }
 
+
 #' Create a subset of mask data
 #'
 #' @param .data data frame
@@ -47,227 +48,7 @@ subset_masks <- function(.data, masks, ...) {
         dplyr::ungroup()
 }
 
-#' Convert a data frame to a time series
-#'
-#' Converts the specified data frame column to a time series using \link[timetk]{tk_ts}.
-#'
-#' @param .data data frame
-#' @param date date column
-#' @param y column to convert to time series
-#' @param .freq the number of observations per unit of time
-#'
-#' @return Returns a \code{ts} object.
-#' @export
-df_to_ts <- function(.data, date, y, .freq = 1) {
-    y <- rlang::enquo(y)
-    date <- rlang::enquo(date)
-
-    .data %>%
-        dplyr::select(!!date, y = !!y) %>%
-        timetk::tk_ts(frequency = .freq, silent = TRUE)
-}
-
-#' Convert a time series to a data frame
-#'
-#' Converts a time series back into a date frame using \link[sweep]{sw_sweep}.
-#'
-#' @param .ts A time-series forecast of class \code{forecast}
-#' @param timetk_idx If timetk index (non-regularized index) is present, uses it
-#'   to develop forecast. Otherwise uses default index.
-#' @param dates vector of dates to replace non-POSIXct indices
-#'
-#' @return Returns a \code{tibble} object.
-#' @export
-ts_to_df <- function(.ts, timetk_idx = TRUE, dates = NULL) {
-    df <- .ts %>%
-        sweep::sw_sweep(timetk_idx = timetk_idx, rename_index = "date") %>%
-        dplyr::mutate(
-            dplyr::across(where(is.POSIXct), lubridate::floor_date, unit = "day"),
-            dplyr::across(where(is.numeric), as.integer)
-        )
-
-    if (!is.POSIXct(df$date) & !is.null(dates)) {
-        df$orig_index <- df$date
-        df$date <- dates
-    }
-
-    df
-}
-
-if(getRversion() >= "2.15.1") utils::globalVariables("where")
-
-#' Get an individual model from a hybridModel object
-#'
-#' @param x The name of the individual model
-#' @param .data A \code{hybridModel} object
-#' @param .idx A vector of dates
-#'
-#' @return Returns a \code{tibble} object.
-#' @keywords internal
-get_mod <- function(x, .data, .idx) {
-    tk_idx = timetk::has_timetk_idx(.data)
-
-    df <- .data[[x]] %>%
-        sweep::sw_sweep(timetk_idx = tk_idx, rename_index = "date") %>%
-        dplyr::filter(key == "forecast")
-
-    if (!is.POSIXct(df$date)) {
-        df <- df %>%
-            dplyr::select(-date) %>%
-            tibble::add_column(date = .idx$date, .before = 1)
-    }
-
-    df
-}
-
-#' Converts a hybrid forecast to a data frame
-#'
-#' Converts a forecast object containing a ensemble (hybrid) forecast and all of
-#' the individual forecasts, into a data frame with the ensemble forecast and
-#' all individual forecasts.
-#'
-#' @param fcast An object of class \code{forecast}
-#' @param mod An object of class \code{hybridModel}
-#' @param y Name of the column that contains the time series values
-#'
-#' @return Returns a \code{tibble} object.
-#' @export
-hyb_to_df <- function(fcast, mod, y) {
-    y <- rlang::enquo(y)
-
-    df <- fcast %>%
-        sweep::sw_sweep(timetk_idx = TRUE, rename_index = "date") %>%
-        dplyr::mutate(
-            dplyr::across(date, lubridate::floor_date, unit = "day"),
-            dplyr::across(key, stringr::str_to_title)
-        )
-
-    idx <- df %>%
-        dplyr::filter(key == "Forecast") %>%
-        dplyr::select(date)
-
-    mod_nm <- names(mod$weights)
-    l <- purrr::map(mod_nm, get_mod, .data = fcast, .idx = idx)
-    names(l) <- mod_nm
-
-    df_mods <- dplyr::bind_rows(l, .id = "key") %>%
-        dplyr::mutate(
-            dplyr::across(
-                key,
-                stringr::str_replace_all,
-                pattern = c(
-                    "auto.arima" = "ARIMA",
-                    "ets" = "ETS",
-                    "nnetar" = "NNAR",
-                    "stlm" = "STL",
-                    "tbats" = "TBATS",
-                    "thetam" = "Theta"
-                )
-            )
-        ) %>%
-        dplyr::arrange(date, key)
-
-    if ("value" %in% names(df_mods)) {
-        df_mods <- df_mods %>%
-            dplyr::mutate(dplyr::across(!!y, ~dplyr::coalesce(., value))) %>%
-            dplyr::select(-value)
-    }
-
-    df %>%
-        dplyr::bind_rows(df_mods) %>%
-        dplyr::arrange(date, key) %>%
-        dplyr::rename(y = !!y) %>%
-        dplyr::mutate(
-            dplyr::across(key, factor),
-            dplyr::across(key, ~fct_relevel(., c("Actual", "Forecast"))),
-            day = as.numeric(difftime(date, first(date), units = "days"))
-        )
-}
-
-#' Make a matrix with external regressors
-#'
-#' Creates a matrix with external regressors for if the date is a weekend, and a
-#' normalized (using \link[timetk]{normalize_vec}) vector of values to be used
-#' in a forecasting model.
-#'
-#' @param .data A data frame
-#' @param date Name of the date column
-#' @param y Name of the column with the external regressors
-#' @param .min The population min value in the normalization process.
-#' @param .max The population max value in the normalization process.
-#'
-#' @return Returns a matrix
-#' @export
-make_preds <- function(.data, date, y, .min = NULL, .max = NULL) {
-    date <- rlang::enquo(date)
-    y <- rlang::enquo(y)
-
-    .data %>%
-        dplyr::mutate(
-            weekend = as.numeric(weekdays(!!date) %in% c("Saturday", "Sunday")),
-            scaled = timetk::normalize_vec(!!y, min = .min, max = .max, silent = TRUE)
-        ) %>%
-        dplyr::select(scaled, weekend) %>%
-        as.matrix()
-}
-
-#' Add smoothed values to data frame
-#'
-#' Applies the \link[stats]{loess} function to time series values and adds the
-#' fitted values to the data frame.
-#'
-#' @param .data A data frame from a \code{forecast} object
-#' @param y_span Parameter which controls the degree of smoothing for trend
-#'   lines
-#' @param pi_span Parameter which controls the degree of smoothing for
-#'   prediction intervals
-#' @param na Action to be taken with missing values in the response or
-#'   predictors. The default is "na.exclude".
-#'
-#' @return Returns a \code{tibble} object.
-#' @export
-make_smooth <- function(.data, key, y_span = 0.75, pi_span = 0.75, na = "na.exclude") {
-    key <- rlang::enquo(key)
-
-    pi <- .data %>%
-        dplyr::filter(stringr::str_to_lower(!!key) != "actual") %>%
-        dplyr::group_by(!!key) %>%
-        tidyr::nest() %>%
-        dplyr::mutate(
-            smth_lo = purrr::map(
-                data,
-                stats::loess,
-                formula = lo.80 ~ day,
-                span = pi_span,
-                na.action = na),
-            fit_lo = purrr::map(smth_lo, `[[`, "fitted"),
-            smth_hi = purrr::map(
-                data,
-                stats::loess,
-                formula = hi.80 ~ day,
-                span = pi_span,
-                na.action = na),
-            fit_hi = purrr::map(smth_hi, `[[`, "fitted")
-        ) %>%
-        tidyr::unnest(cols = c(data, fit_lo, fit_hi)) %>%
-        dplyr::select(date, key, starts_with("fit"))
-
-    .data %>%
-        dplyr::group_by(!!key) %>%
-        tidyr::nest() %>%
-        dplyr::mutate(
-            smth = purrr::map(
-                data,
-                stats::loess,
-                formula = y ~ day,
-                span = y_span,
-                na.action = na),
-            fit = purrr::map(smth, `[[`, "fitted")
-        ) %>%
-        dplyr::select(-smth) %>%
-        tidyr::unnest(cols = c(data, fit)) %>%
-        dplyr::left_join(pi, by = c("date", "key"))
-}
+# if(getRversion() >= "2.15.1") utils::globalVariables("where")
 
 #' Smooth the mean values
 #'
@@ -295,7 +76,7 @@ smooth_mean <- function(.data, key, .col = ".mean", span = 0.5, na.action = "na.
             smth = purrr::map(
                 data,
                 stats::loess,
-                formula = as.formula(paste(.col, "~ day")),
+                formula = stats::as.formula(paste(.col, "~ day")),
                 span = span,
                 na.action = na.action
             ),
@@ -330,7 +111,7 @@ smooth_pi <- function(.data, key, span = 0.75, na.action = "na.exclude") {
             smth_lo = purrr::map(
                 data,
                 stats::loess,
-                formula = as.formula("lo_80 ~ day"),
+                formula = stats::as.formula("lo_80 ~ day"),
                 span = span,
                 na.action = na.action
             ),
@@ -338,7 +119,7 @@ smooth_pi <- function(.data, key, span = 0.75, na.action = "na.exclude") {
             smth_hi = purrr::map(
                 data,
                 stats::loess,
-                formula = as.formula("hi_80 ~ day"),
+                formula = stats::as.formula("hi_80 ~ day"),
                 span = span,
                 na.action = na.action
             ),
--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,191 +1,8 @@
 # plot_functions.R
 
-#' Plot a forecast
+#' Plot forecasts
 #'
-#' @param .data A data frame from a \code{forecast} object
-#' @param .y A string with the column name containing the time series values.
-#' @param actuals A data frame containing the actual (test) values.
-#'
-#' @return A \code{ggplot2} object
-#' @export
-ggmod_plot <- function(.data, .y = "y", actuals = NULL) {
-    p <- ggplot2::ggplot(.data, ggplot2::aes_string(x = "date", y = .y))
-
-    if ("lo.95" %in% names(.data)) {
-        p <- p + ggplot2::geom_ribbon(ggplot2::aes(ymin = lo.95, ymax = hi.95), fill = "grey85", alpha = 0.5)
-    }
-
-    if ("lo.80" %in% names(.data)) {
-        p <- p + ggplot2::geom_ribbon(ggplot2::aes(ymin = lo.80, ymax = hi.80), fill = "grey75", alpha = 0.5)
-    }
-
-    if (!is.null(actuals)) {
-        p <- p + ggplot2::geom_line(data = actuals, color = "black")
-    }
-    p +
-        ggplot2::geom_line(ggplot2::aes(color = key)) +
-        ggplot2::scale_color_manual(NULL, values = c("black", "blue")) +
-        themebg::theme_bg()
-}
-
-#' Add smoothed values to a plot
-#'
-#' Add the smoothed time series values to a \code{plotly} plot.
-#'
-#' @param p A \code{plotly} object
-#' @param .data A data frame from a \code{forecast} object
-#' @param .key Name of the model to add to the plot
-#'
-#' @return A \code{plotly} object
-#' @export
-add_to_plot <- function(p, .data, .key) {
-    df_key <- dplyr::filter(.data, key == .key)
-
-    if (.key %in% c("Actual", "Forecast")) {
-        vis = TRUE
-    } else {
-        vis = "legendonly"
-    }
-
-    if (.key == "Actual") {
-        p %>%
-            plotly::add_markers(
-                y = ~y,
-                data = df_key,
-                alpha = 0.5,
-                marker = list(symbol = "circle-open"),
-                legendgroup = .key,
-                visible = vis
-            ) %>%
-            plotly::add_lines(
-                y = ~fit,
-                data = df_key,
-                name = "Trend",
-                legendgroup = .key,
-                visible = vis
-            )
-    } else {
-        p %>%
-            plotly::add_ribbons(
-                data = df_key,
-                ymin = ~fit_lo,
-                ymax = ~fit_hi,
-                opacity = 0.5,
-                line = list(width = 1),
-                legendgroup = .key,
-                visible = vis,
-                showlegend = FALSE
-            ) %>%
-            plotly::add_lines(
-                y = ~fit,
-                data = df_key,
-                line = list(
-                    # dash = "dash",
-                    width = 1.5
-                ),
-                name = .key,
-                legendgroup = .key,
-                visible = vis
-            )
-    }
-}
-
-#' Add actual values to plot
-#'
-#' @param p A \code{plotly} object
-#' @param .data A data frame from a \code{forecast} object
-#' @param .key Name of the model to add to the plot
-#'
-#' @return A \code{plotly} object
-#' @export
-actuals_to_plot <- function(p, .data, .key) {
-    df_key <- dplyr::filter(.data, key == .key)
-
-    if (.key %in% c("Actual", "Forecast")) {
-        vis = TRUE
-    } else {
-        vis = "legendonly"
-    }
-
-    if (.key == "Actual") {
-        p %>%
-            plotly::add_lines(
-                y = ~y,
-                data = df_key,
-                name = .key,
-                legendgroup = .key,
-                visible = vis
-            )
-    } else {
-        p %>%
-            plotly::add_ribbons(
-                data = df_key,
-                ymin = ~lo.80,
-                ymax = ~hi.80,
-                opacity = 0.5,
-                line = list(width = 1),
-                legendgroup = .key,
-                visible = vis,
-                showlegend = FALSE
-            ) %>%
-            plotly::add_lines(
-                y = ~y,
-                data = df_key,
-                line = list(
-                    # dash = "dash",
-                    width = 1.5
-                ),
-                name = .key,
-                legendgroup = .key,
-                visible = vis
-            )
-    }
-}
-
-#' Plot a hybrid forecast
-#'
-#' Plot a hybrid forecast and all indivudal models using \code{plotly}.
-#'
-#' @param .data A data frame from a \code{forecast} object, with or without smoothed values.
-#' @param .title A string with the plot title.
-#' @param .xtitle A string with the x-axis title.
-#' @param .ytitle A string with the y-axis title.
-#' @param trend If True (default), then the smoothed values are plotted; otherwise plots the actual values.
-#'
-#' @return A \code{plotly} object
-#' @export
-plotly_hybrid <- function(.data,
-                          .title = "Combined forecast model",
-                          .xtitle = "Date",
-                          .ytitle = "Number",
-                          trend = TRUE) {
-
-    keys <- unique(.data$key)
-
-    pal <- RColorBrewer::brewer.pal(length(keys), "Set1")
-    pal[1] <- "#000000"
-
-    p <- plotly::plot_ly(x = ~date, color = ~key, colors = pal) %>%
-        plotly::layout(
-            title = list(text = .title, x = 0),
-            xaxis = list(title = .xtitle, showgrid = FALSE),
-            yaxis = list(title = .ytitle, showgrid = FALSE)
-        )
-
-    for (i in levels(keys)) {
-        if (trend) {
-            p <- add_to_plot(p, .data, i)
-        } else {
-            p <- actuals_to_plot(p, .data, i)
-        }
-    }
-
-    p
-}
-
-#' Plot a hybrid forecast
-#'
-#' Plot a hybrid forecast and all indivudal models using \code{plotly}.
+#' Plots combined forecast model and all indivdual models using \code{plotly}.
 #'
 #' @param actuals A tsibble object containing the known, historical, values
 #' @param y column containing the historical values
@@ -194,6 +11,8 @@ plotly_hybrid <- function(.data,
 #' @param title A string with the plot title.
 #' @param xtitle A string with the x-axis title.
 #' @param ytitle A string with the y-axis title.
+#' @param pi If \code{TRUE} the prediction intervals for all individual models
+#'   will be shown.
 #' @param fc_line A string which Sets the dash style of the forecast lines.
 #'   Valid options are: "solid", "dot", "dash", "longdash", "dashdot", or
 #'   "longdashdot"
@@ -207,16 +26,17 @@ plotly_fable <- function(actuals,
                          title = "Combined forecast model",
                          xtitle = "Date",
                          ytitle = "Number",
+                         pi = TRUE,
                          fc_line = "solid") {
 
     y <- rlang::enquo(y)
-    lo_80 <- as.formula("~lo_80")
-    hi_80 <- as.formula("~hi_80")
-    .mean <- as.formula("~.mean")
-    .model <- as.formula("~.model")
+    lo_80 <- stats::as.formula("~lo_80")
+    hi_80 <- stats::as.formula("~hi_80")
+    .mean <- stats::as.formula("~.mean")
+    .model <- stats::as.formula("~.model")
 
     p <- actuals %>%
-        plotly::plot_ly(x = as.formula("~date"), colors = "Set1") %>%
+        plotly::plot_ly(x = stats::as.formula("~date"), colors = "Set1") %>%
         plotly::layout(
             title = list(text = title, x = 0),
             xaxis = list(title = xtitle, showgrid = FALSE),
@@ -251,18 +71,22 @@ plotly_fable <- function(actuals,
         )
 
     if (!is.null(mods)) {
+        if (pi) {
+            p <- p %>%
+                plotly::add_ribbons(
+                    ymin = lo_80,
+                    ymax = hi_80,
+                    data = mods,
+                    color = .model,
+                    opacity = 0.4,
+                    line = list(width = 1),
+                    legendgroup = .model,
+                    visible = "legendonly",
+                    showlegend = FALSE
+                )
+        }
+
         p <- p %>%
-            plotly::add_ribbons(
-                ymin = lo_80,
-                ymax = hi_80,
-                data = mods,
-                color = .model,
-                opacity = 0.4,
-                line = list(width = 1),
-                legendgroup = .model,
-                visible = "legendonly",
-                showlegend = FALSE
-            ) %>%
             plotly::add_lines(
                 y = .mean,
                 data = mods,
@@ -285,9 +109,6 @@ plotly_fable <- function(actuals,
 #' @param title A string with the plot title.
 #' @param xtitle A string with the x-axis title.
 #' @param ytitle A string with the y-axis title.
-#' @param fc_line A string which Sets the dash style of the forecast lines.
-#'   Valid options are: "solid", "dot", "dash", "longdash", "dashdot", or
-#'   "longdashdot"
 #'
 #' @return A \code{plotly} object
 #' @export
@@ -296,15 +117,15 @@ plotly_smooth <- function(.data,
                          xtitle = "Date",
                          ytitle = "Number") {
 
-    fit_lo <- as.formula("~fit_lo")
-    fit_hi <- as.formula("~fit_hi")
-    fit <- as.formula("~fit")
-    .model <- as.formula("~.model")
-    .mean <- as.formula("~.mean")
+    fit_lo <- stats::as.formula("~fit_lo")
+    fit_hi <- stats::as.formula("~fit_hi")
+    fit <- stats::as.formula("~fit")
+    .model <- stats::as.formula("~.model")
+    .mean <- stats::as.formula("~.mean")
 
     .data %>%
         plotly::plot_ly(
-            x = as.formula("~date"),
+            x = stats::as.formula("~date"),
             color = .model,
             colors = c("#000000", "#377eb8")) %>%
         plotly::layout(
