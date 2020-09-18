fc2a642eb57de6558052540162c9e3b9bdc5bca2
Brian Gulbis <brian.gulbis@gmail.com>
2020-09-17 19:49:31 -0500

    revise for multiple models

--- R/data_functions.R
+++ R/data_functions.R
@@ -7,18 +7,22 @@
 #' @param .data A data frame
 #' @param .col The column with the value
 #' @param m A string with the name of the mask
+#' @param model A string with the name of the forecast model to use, defaults
+#'   to "Forecast" which is the ensemble model
 #' @param .date A POSIXct date
 #'
 #' @return A numeric value
 #' @export
-get_value <- function(.data, .col, m, .date = lubridate::today()) {
+get_value <- function(.data, .col, m, model = "Forecast", .date = lubridate::today()) {
     .col <- rlang::enquo(.col)
     mask <- rlang::sym("mask")
     date <- rlang::sym("date")
+    .model <- rlang::sym(".model")
 
     .data %>%
         dplyr::filter(
             !!mask == m,
+            !!.model == model,
             !!date == .date
         ) %>%
         dplyr::pull(!!.col)
@@ -29,43 +33,40 @@ get_value <- function(.data, .col, m, .date = lubridate::today()) {
 #' Calculate the expected inventory, burn rate, days remaining, and
 #' days-on-hand.
 #'
-#' @param .data A data frame
+#' @param .data A data frame with historical values
+#' @param .xreg A data frame with forecast values
 #' @param .col Column with the number of masks distributed
 #'
 #' @return Returns a \code{tibble} object.
 #' @export
-inventory_calcs <- function(.data, .col) {
+inventory_calcs <- function(.data, .xreg, .col) {
     .col <- rlang::enquo(.col)
+    date <- rlang::sym("date")
     shipments <- rlang::sym("shipments")
-    lag_ship <- rlang::sym("lag_ship")
-    lag_dist <- rlang::sym("lag_dist")
     stock_change <- rlang::sym("stock_change")
-    .model <- rlang::sym(".model")
+    data_type <- rlang::sym("data_type")
     new_inventory <- rlang::sym("new_inventory")
-    tmp_inv <- rlang::sym("tmp_inv")
+    reprocessed <- rlang::sym("reprocessed")
     cum_change <- rlang::sym("cum_change")
     available <- rlang::sym("available")
     burn_rate <- rlang::sym("burn_rate")
-    days_remain <- rlang::sym("days_remain")
 
     .data %>%
+        dplyr::bind_rows(.xreg) %>%
+        dplyr::arrange(!!date) %>%
+        tidyr::fill(!!new_inventory, .direction = "down") %>%
         dplyr::mutate(
-            dplyr::across(c(!!shipments, !!.col), round),
-            !!"lag_dist" := dplyr::lag(!!.col),
-            !!"stock_change" := !!lag_ship - !!lag_dist,
-            dplyr::across(!!stock_change, ~if_else(!!.model == "Actual", 0, .)),
+            dplyr::across(reprocessed, ~dplyr::if_else(date >= lubridate::mdy("9/5/2020"), 0, .)),
+            dplyr::across(c(!!shipments, !!.col), dplyr::lag),
+            !!"stock_change" := !!shipments - !!.col,
+            dplyr::across(!!stock_change, ~if_else(!!data_type == "Actual", 0, .)),
             !!"cum_change" := cumsum(!!stock_change),
-            dplyr::across(!!new_inventory, ~dplyr::coalesce(., !!tmp_inv + !!cum_change)),
-            dplyr::across(!!available, ~dplyr::coalesce(., !!new_inventory)),
+            !!"new_inventory" := !!new_inventory + !!cum_change,
+            !!"available" := !!new_inventory + !!reprocessed,
             !!"burn_rate" := (dplyr::lag(!!available, 7) - !!available) / 7,
-            # !!"avg_burn_rate" := zoo::rollmean(!!burn_rate, 7, fill = "extend"),
-            !!"days_remain" := !!available / !!burn_rate,
-            # !!"days_remain" := !!available / zoo::rollmean(!!burn_rate, 7, fill = "extend"),
-            # dplyr::across(!!days_remain, ~dplyr::if_else(. < 0, NA_real_, .)),
-            !!"days_on_hand" := !!available / zoo::rollmean(!!.col, 7, fill = "extend")
-            # !!"days_on_hand" := zoo::rollmean(!!available, 7, fill = "extend") / zoo::rollmean(!!.col, 7, fill = "extend")
+            # !!"days_remain" := !!available / !!burn_rate,
+            !!"days_on_hand" := !!available / zoo::rollapplyr(!!.col, 7, mean, partial = TRUE)
         )
-
 }
 
 #' Smooth the mean values
